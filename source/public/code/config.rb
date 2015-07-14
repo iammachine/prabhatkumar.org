83e2657722e5691b3fb8496ec37788541436ac6b
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 00:38:02 +0530

    initial config.rb - commit.
    @config.rb --> for Sass and Compass, via Ruby.

---
+++ source/public/code/config.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# prabhatkumar.org [v0.0.1]
+# @author    : Prabhat Kumar
+# @copyright : Prabhat Kumar
+# @date      : 14-July-2015
+# @require   : Ruby [v2.2.2]
+# @require   : Sass - Selective Steve [3.4.15]
+# @require   : Compass - Polaris [1.0.3]
+# ============================================
