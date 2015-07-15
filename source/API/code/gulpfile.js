99a2abccd2c5f30471470be28165834cf1368fd0
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 17:26:13 +0530

    initial gulpfile.js - commit.
    Node.js®, Gulp and required modules.
    http://nodejs.org and http://gulpjs.com/

---
+++ source/API/code/gulpfile.js
@@ -0,0 +1,13 @@
+/*!
+ * api.prabhatkumar.org® [v0.0.1]
+ * _____________________________________________________
+ * Gulp: The streaming build system.
+ * https://github.com/gulpjs/gulp | http://gulpjs.com/
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ * @copyright : Prabhat Kumar [http://prabhatkumar.org/]
+ * @date      : 15-July-2015
+ * _____________________________________________________
+ * @require   : Node.js®
+ * @require   : Gulp
+ * _____________________________________________________
+ */
