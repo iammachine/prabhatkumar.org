91e9b7facef4901ef1ba5492c66b2a37c161f094
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 17:35:59 +0530

    update of gulpfile.js - commit.
    @require --> gulp modules.
    https://github.com/wearefractal/gulp-coffee or https://www.npmjs.com/package/coffee-gulp

--- source/API/code/gulpfile.js
+++ source/API/code/gulpfile.js
@@ -17,3 +17,22 @@
 
 /* @Invoking strict mode */
 "use strict";
+
+// main module.
+var gulp                   = require('gulp');
+var coffee          = require('gulp-coffee');
+var concat          = require('gulp-concat');
+var uglify          = require('gulp-uglify');
+var beautify      = require('gulp-beautify');
+var sourcemaps  = require('gulp-sourcemaps');
+// utility modules.
+var path                   = require('path');
+var gutil             = require('gulp-util');
+var gulpif              = require('gulp-if');
+var size              = require('gulp-size');
+var del                     = require('del');
+var notify          = require("gulp-notify");
+var rename          = require("gulp-rename");
+var replace        = require('gulp-replace');
+var bytediff      = require('gulp-bytediff');
+var chmod            = require('gulp-chmod');
