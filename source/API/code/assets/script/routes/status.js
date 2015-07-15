994540cb4fb2c40fdb0af5e57fd46d52e5c7b134
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 16:05:51 +0530

    update of status.js - commit.
    @function --> nsp.on added.

--- source/API/code/assets/script/routes/status.js
+++ source/API/code/assets/script/routes/status.js
@@ -13,3 +13,33 @@ var io    = global.socket;
 var usage = require('usage');
 var os    = require('os');
 var merge = require('utils-merge');
+
+// @nsp.on - socket status...
+var nsp = io.of('/status');
+
+nsp.on('connection', function(socket) {
+  /*! Emit status. */
+  function emitStatus() {
+    var pid = process.pid;
+    var options = {
+      keepHistory: true
+    };
+    var status = {
+      loadAvg: os.loadavg()
+    };
+    usage.lookup(pid, options, function(err, result) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      merge(status, result);
+      socket.emit('status', status);
+    });
+  }
+  emitStatus();
+  var intervalID = setInterval(emitStatus, 1000 * 5);
+  // @disconnect...
+  socket.on('disconnect', function() {
+    clearInterval(intervalID);
+  });
+});
