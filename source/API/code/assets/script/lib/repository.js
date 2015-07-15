b2593b25c527bd3a400776776254f983573204ea
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 14:58:27 +0530

    update of repository.js - commit.
    @function --> build added.

--- source/API/code/assets/script/lib/repository.js
+++ source/API/code/assets/script/lib/repository.js
@@ -70,3 +70,54 @@ function copy(cwd, outputPath) {
     });
   };
 }
+
+function build(repository, outputPath) {
+  console.log('Building [prabhatkumar.org]' + repository + '...');
+  var dir = path.resolve(process.cwd(), repository);
+  fs.exists(dir + '/bower.json', function(bower) {
+    async.series([
+      execSeries('git checkout .', dir),
+      bower ? execSeries('bower prune --allow-root', dir) : noop,
+      execSeries('npm prune', dir),
+      bower ? execSeries('bower install --allow-root', dir) : noop,
+      execSeries('npm install', dir),
+      execSeries('grunt', dir),
+      clean(outputPath),
+      copy(dir + '/dist', outputPath)
+    ], function(err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  });
+}
+
+exports.build = build;
+
+exports.onPostReceive = function(repository, callback) {
+  console.log('Updating [prabhatkumar.org]' + repository + '...');
+  var dir = path.resolve(process.cwd(), repository);
+  fs.exists(dir, function(exists) {
+    if (!exists) {
+      callback(new Error('Not Found!'));
+      return;
+    }
+    async.series([
+      execSeries('git checkout .', dir),
+      execSeries('git pull -v --no-rebase --progress  "origin"', dir)
+      ], function(err) {
+        if (err) {
+          callback(err);
+          return;
+        }
+      for (var i = 0; i < webapps.length; i++) {
+        if (repository === webapps[i]) {
+          var outputPath = 'prabhatkumar.org/' + repository;
+          dir = path.resolve(process.cwd(), outputPath);
+          build(repository, dir);
+        }
+      }
+      callback(null);
+    });
+  });
+};
