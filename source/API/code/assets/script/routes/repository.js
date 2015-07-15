c2b7e735544aca8fbdf70a8ba3ec68bb519c8ab8
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 15:38:52 +0530

    update of repository.js - commit.
    @function --> app.post added.

--- source/API/code/assets/script/routes/repository.js
+++ source/API/code/assets/script/routes/repository.js
@@ -12,3 +12,24 @@
 var app        = require('../app');
 var passport   = require('passport');
 var repository = require('../lib/repository');
+
+app.post('/repository/:name', passport.authenticate('basic', { session: false }), function(req, res) {
+  var name = req.params.name;
+  if (!name) {
+    res.sendStatus(400);
+    return;
+  }
+  // @onPostReceive...
+  repository.onPostReceive(name, function(err) {
+    if (err) {
+      switch (err.message) {
+        case 'Not Found!':
+          res.sendStatus(404);
+          break;
+      }
+      return;
+    }
+    res.sendStatus(200);
+  });
+});
+
