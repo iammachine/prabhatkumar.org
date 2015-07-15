0e2eaa691baeb4f1663d6899b976869dec58e2fb
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 13:42:19 +0530

    initial index.js - commit.

---
+++ source/API/code/assets/script/config/index.js
@@ -0,0 +1,5 @@
+/*!
+ * api.prabhatkumar.org® [v0.0.1]
+ * @author    : Prabhat Kumar [http://prabhatkumar.org/]
+ */
+module.exports = require('./' + (process.env.NODE_ENV || 'development') + '.json');
