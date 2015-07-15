e8484c3c6412467f6059efec4f58b2536c207ff2
Prabhat Kumar <iammachine@users.noreply.github.com>
2015-07-15 13:38:06 +0530

    update of app.js - commit.
    @require --> modules.

--- source/API/code/assets/script/app.js
+++ source/API/code/assets/script/app.js
@@ -7,3 +7,13 @@
 
 /* @Invoking strict mode */
 "use strict";
+
+/* API server for api.prabhatkumar.org/ */
+var http          = require('http');
+var https         = require('https');
+var fs            = require('fs');
+var express       = require('express');
+var passport      = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+var forceSSL      = require('express-force-ssl');
+var bodyParser    = require('body-parser');
